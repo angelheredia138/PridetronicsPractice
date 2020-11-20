715bb3103170b98c14a7f2ef594c4a7da6361f1c
angelheredia138 <herediafangel@gmail.com>
2020-11-20 14:32:51 -0700

    added all the problems

--- test/test/Test.java
+++ test/test/Test.java
@@ -1,9 +1,12 @@
 package test;
 
+import java.util.Scanner;
+
 class Test {
     public static void main(String[] args) {
-        
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        Scanner nameScanner = new Scanner(System.in); 
+        String fullName = nameScanner.nextLine(); 
+        System.out.println(fullName);
+
     }
 }
